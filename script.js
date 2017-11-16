703109b00b6a58f1c292c4f19e27cb8ab2388484
NamelessKing <namelessking@email.com>
2017-11-16 20:40:56 +0100

    jqueryUI experiments

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
             function isEmail(email) {
-                var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-                return regex.test(email);
+              var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+              return regex.test(email);
             }
-            
-            
-            $("#submitButton").click(function(){
-                
-                var errorMessage = "";
-                var fieldsMissing ="";
-                
-                if($("#email").val() == ""){
-                    fieldsMissing += "<br>Email";
-                };
-                
-                if($("#telephone").val() == ""){
-                    fieldsMissing += "<br>Telephone";
-                };
-                
-                if($("#password").val() == ""){
-                    fieldsMissing += "<br>Password";
-                };
-                
-                if($("#passwordConfirm").val() == ""){
-                    fieldsMissing += "<br>Confirme Password";
-                };
-                
-                if(fieldsMissing != ""){
-                   errorMessage += "<p>The following field(s) are missing: "+fieldsMissing;
-                }
-                
-                if(!isEmail($("#email").val())){
-                    errorMessage += "<p>Your email address is not valid</p>"
-                };
-                
-                if(!$.isNumeric($("#telephone").val())){
-                   errorMessage += "<p>Number non correct</p>"
-                }
-                
-                if($("#password").val() != $("#passwordConfirm").val()){
-                   errorMessage += "<p>Your passwords don't match</p>"
-                }
-                
-                if(errorMessage != ""){
-                    $("#errorMessage").html(errorMessage);   
-                }else{
-                    $("#successMessage").show();
-                    $("#errorMessage").hide();
-                }
-            
+
+
+            $("#submitButton").click(function() {
+
+              var errorMessage = "";
+              var fieldsMissing = "";
+
+              if ($("#email").val() == "") {
+                fieldsMissing += "<br>Email";
+              };
+
+              if ($("#telephone").val() == "") {
+                fieldsMissing += "<br>Telephone";
+              };
+
+              if ($("#password").val() == "") {
+                fieldsMissing += "<br>Password";
+              };
+
+              if ($("#passwordConfirm").val() == "") {
+                fieldsMissing += "<br>Confirme Password";
+              };
+
+              if (fieldsMissing != "") {
+                errorMessage += "<p>The following field(s) are missing: " + fieldsMissing;
+              }
+
+              if (!isEmail($("#email").val())) {
+                errorMessage += "<p>Your email address is not valid</p>"
+              };
+
+              if (!$.isNumeric($("#telephone").val())) {
+                errorMessage += "<p>Number non correct</p>"
+              }
+
+              if ($("#password").val() != $("#passwordConfirm").val()) {
+                errorMessage += "<p>Your passwords don't match</p>"
+              }
+
+              if (errorMessage != "") {
+                $("#errorMessage").html(errorMessage);
+              } else {
+                $("#successMessage").show();
+                $("#errorMessage").hide();
+              }
+
             });
